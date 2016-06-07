a61e74c9d1bbee85b329d48310b14ff32af162ef
Denzel Christopher Adams <Blvckyuppie@gmail.com>
2016-06-07 13:29:15 -0400

    HTML reasonably responsive, wrote 1st array

--- tg.js
+++ tg.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-alert("The Javascript is linked")
+  var quizQ = [
+    {
+      question: "On Top Gear series 8 episode 1, which presenter said this quote? 'This is a Renault Espace, probably the best of the people carriers. Not that thats much to shout about. Thats like saying 'Oh good, Ive got syphilis, the best of the sexually transmitted diseases!'",
+      answer: "Jeremy Clarkson"
+    },
+    {
+      question: "...",
+      answer: "..."
+    }
+  ]
 });
